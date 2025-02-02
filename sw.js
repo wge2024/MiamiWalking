4128739457f1151db8080361c717d0edd5a1be00
WGE <wge2024@gmail.com>
2025-02-02 13:48:35 -0500

    Changed to new Service Worker and changed body scripts on each page

--- sw.js
+++ sw.js
@@ -1,20 +1,49 @@
+// Install event listener to cache resources
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/CharityWalks.html',
+        '/ComfortShoes.html',
+        '/ExerciseShoes.html/',
+        '/MensCasualShoes.html',
+        '/MensExerciseShoes.html',
+        '/MensShoes.html',
+        '/WomensCasualShoes.html',
+        '/WomensExerciseShoes.html',
+        '/WomensShoes.html',
+        '/styles.css',
+        '/script.js',
+        '/images/logo.png',
+        '/favicon.ico'
+        // Add more files as needed
+      ]);
+    })
+  );
+});
+
+// Activate event listener to clean up old caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== 'my-cache') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event listener to serve from cache or network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
